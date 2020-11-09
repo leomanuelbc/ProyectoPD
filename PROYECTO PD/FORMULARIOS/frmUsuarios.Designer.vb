97d1fba26a29908f2dc5b7ebd80163a1a0a0e21b
Manuel <Leomanuelbc@gmail.com>
2020-11-09 00:08:00 -0500

    INSERCION DE NUEVOS USUARIOS CON EL FORMULARIO

---
+++ PROYECTO PD/FORMULARIOS/frmUsuarios.Designer.vb
@@ -0,0 +1,289 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUsuarios
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtContraseña = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cboTipoUser = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.cboEstado = New System.Windows.Forms.ComboBox()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.BtnCancelar = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblTotalUsuarios = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre Completo"
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Location = New System.Drawing.Point(120, 19)
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(202, 20)
+        Me.txtnombre.TabIndex = 1
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Location = New System.Drawing.Point(120, 56)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(119, 20)
+        Me.txtUsuario.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Usuario"
+        '
+        'txtContraseña
+        '
+        Me.txtContraseña.Location = New System.Drawing.Point(120, 96)
+        Me.txtContraseña.Name = "txtContraseña"
+        Me.txtContraseña.Size = New System.Drawing.Size(119, 20)
+        Me.txtContraseña.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Contraseña"
+        '
+        'cboTipoUser
+        '
+        Me.cboTipoUser.FormattingEnabled = True
+        Me.cboTipoUser.Items.AddRange(New Object() {"Administrador", "Invitado"})
+        Me.cboTipoUser.Location = New System.Drawing.Point(120, 136)
+        Me.cboTipoUser.Name = "cboTipoUser"
+        Me.cboTipoUser.Size = New System.Drawing.Size(159, 21)
+        Me.cboTipoUser.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(21, 144)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Tipo de Usuario"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(21, 184)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Estado"
+        '
+        'cboEstado
+        '
+        Me.cboEstado.FormattingEnabled = True
+        Me.cboEstado.Items.AddRange(New Object() {"Activo", "Inactivo"})
+        Me.cboEstado.Location = New System.Drawing.Point(120, 176)
+        Me.cboEstado.Name = "cboEstado"
+        Me.cboEstado.Size = New System.Drawing.Size(159, 21)
+        Me.cboEstado.TabIndex = 9
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(24, 221)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(85, 35)
+        Me.btnNuevo.TabIndex = 10
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(115, 221)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(85, 35)
+        Me.btnGuardar.TabIndex = 11
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Location = New System.Drawing.Point(206, 221)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(85, 35)
+        Me.btnEditar.TabIndex = 12
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(72, 270)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(85, 35)
+        Me.btnEliminar.TabIndex = 13
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'BtnCancelar
+        '
+        Me.BtnCancelar.Location = New System.Drawing.Point(163, 270)
+        Me.BtnCancelar.Name = "BtnCancelar"
+        Me.BtnCancelar.Size = New System.Drawing.Size(85, 35)
+        Me.BtnCancelar.TabIndex = 14
+        Me.BtnCancelar.Text = "Cancelar"
+        Me.BtnCancelar.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lblTotalUsuarios)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.DataGridView1)
+        Me.Panel1.Location = New System.Drawing.Point(339, 19)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(449, 284)
+        Me.Panel1.TabIndex = 15
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(11, 51)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(427, 201)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(239, 17)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(166, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(16, 17)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 13)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Buscar"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(11, 259)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(113, 13)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "Usuarios Registrados :"
+        '
+        'lblTotalUsuarios
+        '
+        Me.lblTotalUsuarios.AutoSize = True
+        Me.lblTotalUsuarios.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalUsuarios.Location = New System.Drawing.Point(120, 257)
+        Me.lblTotalUsuarios.Name = "lblTotalUsuarios"
+        Me.lblTotalUsuarios.Size = New System.Drawing.Size(44, 16)
+        Me.lblTotalUsuarios.TabIndex = 4
+        Me.lblTotalUsuarios.Text = "Total"
+        '
+        'frmUsuarios
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 313)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.BtnCancelar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.cboEstado)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cboTipoUser)
+        Me.Controls.Add(Me.txtContraseña)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtnombre)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmUsuarios"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Registro de Usuarios"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtnombre As TextBox
+    Friend WithEvents txtUsuario As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtContraseña As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cboTipoUser As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents cboEstado As ComboBox
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents BtnCancelar As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblTotalUsuarios As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ PROYECTO PD/FORMULARIOS/frmUsuarios.vb
@@ -0,0 +1,90 @@
+﻿Imports System.Data.SqlClient
+Public Class frmUsuarios
+    Private Sub frmUsuarios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call desactivarControles()
+
+    End Sub
+    Sub desactivarControles()
+        btnGuardar.Enabled = False
+        btnEditar.Enabled = False
+        btnEliminar.Enabled = False
+        BtnCancelar.Enabled = False
+
+        txtnombre.Enabled = False
+        txtContraseña.Enabled = False
+        txtUsuario.Enabled = False
+        cboTipoUser.Enabled = False
+        cboEstado.Enabled = False
+
+        btnNuevo.Enabled = True
+    End Sub
+    Sub activarControles()
+        btnGuardar.Enabled = True
+        btnEditar.Enabled = False
+        btnEliminar.Enabled = False
+        BtnCancelar.Enabled = True
+
+        txtnombre.Enabled = True
+        txtContraseña.Enabled = True
+        txtUsuario.Enabled = True
+        cboTipoUser.Enabled = True
+        cboEstado.Enabled = True
+
+        btnNuevo.Enabled = False
+    End Sub
+    Sub limpiarControles()
+        txtnombre.Text = ""
+        txtUsuario.Text = ""
+        'txtContraseña.Text = "" -- es lo mismo el clear lo dejamos para probar funcionalidades
+        txtContraseña.Clear()
+        cboTipoUser.Text = ""
+        cboEstado.Text = ""
+    End Sub
+    Sub insertar()
+        Dim sql As String
+        Dim id As Integer
+        If MsgBox("Guardar Nuevo Registro", vbQuestion + vbYesNo, "Sistema") = vbNo Then
+            Exit Sub
+
+
+        End If
+        adaptador = New SqlDataAdapter("Select * from Usuarios where usuario ='" & txtUsuario.Text & "' ", obtenerconexion)
+        tabla.Clear()
+        adaptador.Fill(tabla)
+        If tabla.Rows.Count > 0 Then
+            txtUsuario.Text = tabla.Rows(0).Item("Usuario")
+            MsgBox("El Registro Ya Existe en la Base de Datos", vbInformation, "Sistema")
+            Exit Sub
+        End If
+        If txtnombre.Text = "" Or txtUsuario.Text = "" Or txtContraseña.Text = "" Or cboTipoUser.Text = "" Or cboEstado.Text = "" Then
+            MsgBox("Existen Campos Vacios", vbInformation, "Sistema")
+            Exit Sub
+        Else
+            sql = "INSERT INTO Usuarios(NombreCompleto,Usuario,Contraseña,TipoUsuario,Estado) VALUES('" & txtnombre.Text & "','" & txtUsuario.Text & "','" & txtContraseña.Text & "','" & cboTipoUser.Text & "','" & cboEstado.Text & "')"
+            Dim conect As New SqlConnection(obtenerconexion)
+            conect.Open()
+            Using comando As New SqlCommand(sql, conect)
+                id = comando.ExecuteScalar()
+            End Using
+            conect.Close()
+            MsgBox("Registro Completado", vbInformation, "Sistema")
+        End If
+    End Sub
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        'Tambien puedeo solo poner el activar controles y asi llama al proceso no es necesario el call
+        'pero lo dejo por que se ve mejor el llamar al proceso  
+        Call activarControles()
+        Call limpiarControles()
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Call insertar()
+        Call desactivarControles()
+        Call limpiarControles()
+    End Sub
+
+    Private Sub BtnCancelar_Click(sender As Object, e As EventArgs) Handles BtnCancelar.Click
+        Call desactivarControles()
+        Call limpiarControles()
+    End Sub
+End Class
